c2965e49e8d1600e9496a930b170732b9c14a17f
drake-horne <drakehornevan@gmail.com>
2022-05-05 12:04:28 -0700

    Changed database and created music app

--- music_project/settings.py
+++ music_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d&rw!j05!xa=blp$672u72g-_p%&_pxbgg8!v**3#o%+y2%kv='
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,15 +72,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'music_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -124,3 +115,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from music_project.local_settings import *
+except ImportError:
+    pass
