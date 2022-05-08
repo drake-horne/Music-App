060ad4883b4049aeb12c32d804f7bd740b94a7ce
drake-horne <drakehornevan@gmail.com>
2022-05-07 17:56:24 -0700

    Fixed mistake in models and serializers and exported post man requests

--- music/models.py
+++ music/models.py
@@ -1,3 +1,4 @@
+from audioop import maxpp
 from django.db import models
 
 # Create your models here.
@@ -6,6 +7,6 @@ class Music(models.Model):
     title=models.CharField(max_length=255)
     artist=models.CharField(max_length=255)
     album=models.CharField(max_length=255)
-    release_date=models.DateField(auto_now=True)
+    release_date=models.DateField(max_length=255)
     genre=models.CharField(max_length=255)
 
